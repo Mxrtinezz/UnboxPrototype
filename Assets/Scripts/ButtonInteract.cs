6f38cfb58ef41867ca2efb97d1eadefb6f229add
MX <shadowmario1@gmail.com>
2023-06-21 00:08:54 +1000

    Button Interact Progress
    Feature (controls) #5
    - Made progress on making the button interaction work, however it is still unfinished and unable to work.
    - Changes include class references from another script, and new functions.

--- Assets/Scripts/ButtonInteract.cs
+++ Assets/Scripts/ButtonInteract.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using System;
+using System.Runtime.CompilerServices;
 
 /*
 For buttons, instead of using Event Systems, take the "Interactable" script from zombie game, and make remove the Event thing.
@@ -12,7 +14,7 @@ Change it to make it so that, if it's clicked, it just checks if a function of b
 
 public class ButtonInteract : MonoBehaviour
 {
-    // Raycast setup
+    // Raycast Setup Variables
     [Header("Raycast Settings")]
     private Ray b_ray = new Ray(); // Define a ray for this check
     private RaycastHit b_hitObject; // Use the RaycastHit type to get an object hit
@@ -23,12 +25,18 @@ public class ButtonInteract : MonoBehaviour
     public UnityEvent b_onObjectClicked; // store a callback event to some other function
     public Image CrosshairDot;
 
-    // Raycast Results - For SHOWING bool results, not setting them
+    // Raycast Results Variables - For SHOWING bool results, not setting them
     [Header("Results")]
-    public bool b_didHit;
+    public bool b_didHit; // Shows if the raycast did actually hit the collider
     public bool b_canInteract;
     public GameObject interactiveObject;
     public bool targetIsInteractive;
+
+    // Prompt System Variables
+    [Header("Prompt Options")]
+    public bool interactablePromptsText; // These are all a checkbox to see IF the player wants a popup or not.
+    public Text intPromptText;
+    public string intMessage;
 
     void Start()
     {
@@ -44,11 +52,17 @@ public class ButtonInteract : MonoBehaviour
         {
             if (b_hitObject.collider.tag == "Button") // If that collider has the "Button" tag
             {
+                // This stuff is about firing prompts for something you can interact with in the world (in this case, button)
                 b_didHit = true;
                 interactiveObject = b_hitObject.collider.gameObject;
                 targetIsInteractive = true;
+                if (interactablePromptsText)
+                {
+                    intPromptText.enabled = true;
+                    intPromptText.text = intMessage;
+                }
             }
-            else
+            else // If raycast doesn't hit one of those things, reset raycast and deactivate all prompts
             {
                 b_didHit = false;
                 interactiveObject = null;
@@ -61,16 +75,27 @@ public class ButtonInteract : MonoBehaviour
             CrosshairDot.GetComponent<Renderer>();
             CrosshairDot.color = Color.green;
             b_canInteract = true;
+            targetIsInteractive = false;
+            intPromptText.enabled = false;
         }
 
-
-    }
-
-    private void ButtonPress()
-    {
-        if (b_isHit == true)
+        if (b_didHit == false) // Resets colour of crosshair
         {
-
+            CrosshairDot.GetComponent<Renderer>();
+            CrosshairDot.color = Color.white;
+            b_canInteract = false;
         }
+
+        // INTERACTIONS - This is where the ACTUAL interactions happen, and result in calling the relative functions
+
+        if (b_canInteract == true)
+        {
+            if (targetIsInteractive)
+            {
+                interactiveObject.GetComponent<Interactable>().ButtonPress(); 
+            }
+        }
+
+
     }
 }
