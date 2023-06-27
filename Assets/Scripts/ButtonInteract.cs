1d66858b0a91f9066d4397e084e58c050b27fbef
MX <shadowmario1@gmail.com>
2023-06-27 18:00:11 +1000

    Final Submission Commit
    Feature (controls) #15
    - Refined the ButtonInteract script to condense and simplify it.

--- Assets/Scripts/ButtonInteract.cs
+++ Assets/Scripts/ButtonInteract.cs
@@ -8,93 +8,49 @@ using System.Runtime.CompilerServices;
 
 public class ButtonInteract : MonoBehaviour
 {
-    // Raycast Setup Variables
-    [Header("Raycast Settings")]
-    private Ray b_ray = new Ray(); // Define a ray for this check
-    private RaycastHit b_hitObject; // Use the RaycastHit type to get an object hit    
-    public LayerMask b_layerToHit; // Defining a layer that will be detected with our raycast
+    // Raycast Setup Variables    
+    private Ray b_ray = new Ray(); // Defines the button raycast
+    private RaycastHit b_hitObject; // Checks if the ray has hit an object    
     public float b_rayLength = 5f; // Length of the ray
-    public KeyCode b_boundKey; // E
-    public UnityEvent b_onObjectClicked; // store a callback event to some other function
-    public Image CrosshairDot;
+    public KeyCode b_boundKey; // Left Mouse Button (Mouse 0 in Unity)
+    public Image Crosshair;
+    public GameObject buttonObject; // The actual button game object in unity
+    public bool b_didHit; // A bool to show if the raycast hit the button - mostly for debugging
 
-    // Raycast Results Variables - For SHOWING bool results, not setting them
-    [Header("Results")]
-    public bool b_didHit; // Shows if the raycast did actually hit the collider
-    public bool b_canInteract; // Can the player interact with the object?
-    public GameObject interactiveObject; // The actual button
-    public bool targetIsInteractive; // Is the thing the ray hit an interactive object?
-
-    // Prompt System Variables
-    [Header("Prompt Options")]
-    public bool interactablePromptsText; // These are all a checkbox to see IF the player wants a popup or not.
-    public Text intPromptText;
-    public string intMessage;
-
-    // Start is called before the frame update
     void Start()
     {
-        b_didHit = false;
-        b_canInteract = false;
-        CrosshairDot = GameObject.Find("CrosshairDot").GetComponent<Image>(); // Finds the CrosshairDot UI object in Unity
+        Crosshair = GameObject.Find("CrosshairDot").GetComponent<Image>(); // Sets up the Crosshair UI for the player
     }
 
-    // Update is called once per frame
     void Update()
     {
-        b_ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Raycast from mouse position
-        if (Physics.Raycast(b_ray, out b_hitObject, b_rayLength, b_layerToHit)) // If raycast hits collider...
+        b_ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Sets the ray start point and direction
+        if (Physics.Raycast(b_ray, out b_hitObject, b_rayLength)) // Raycast from where the camera is looking
         {
-            if (b_hitObject.collider.tag == "Interact") // If that collider has the "Interact" tag
-            {
-                // This stuff is about firing prompts for something you can interact with in the world (in this case, button (interactive object))
-                b_didHit = true;
-                interactiveObject = b_hitObject.collider.gameObject; // The interactive object is the button's collider
-                targetIsInteractive = true;
-                if (interactablePromptsText == true)
+            if (b_hitObject.collider.tag == "Button") // If raycast hits collider...
+            {                
+                //Debug.Log("Button ray hit");
+
+                // Turns crosshair green
+                Crosshair.GetComponent<Renderer>();
+                Crosshair.color = Color.green;
+
+                buttonObject = b_hitObject.collider.gameObject; // Makes the button's collider interactive
+
+                // If the player clicks Left Mouse Button, activate the button
+                if (Input.GetKeyDown(b_boundKey))
                 {
-                    intPromptText.enabled = true;
-                    intPromptText.text = intMessage;
+                    buttonObject.GetComponent<Interactable>().ButtonPress(); // Calls the scripts that activate the button and open the door
                 }
-            }
-            else // If raycast doesn't hit one of those things, reset raycast and deactivate all prompts
-            {
-                b_didHit = false;
-            }
+            }            
         }
         else
         {
-            b_didHit = false;
-        }
+            // Turns crosshair back to white when the ray isn't hitting the button
+            Crosshair.GetComponent<Renderer>();
+            Crosshair.color = Color.white;
 
-        if (b_didHit == true) // Turns crosshair green
-        {
-            CrosshairDot.GetComponent<Renderer>();
-            CrosshairDot.color = Color.green;
-            b_canInteract = true;
-            //targetIsInteractive = false;
-            intPromptText.enabled = false;
-        }
-
-        if (b_didHit == false) // Resets colour of crosshair
-        {
-            CrosshairDot.GetComponent<Renderer>();
-            CrosshairDot.color = Color.white;
-            b_canInteract = false;
-            interactiveObject = null;
-        }
-
-        // INTERACTIONS - This is where the ACTUAL interactions happen, and result in calling the relative functions
-
-        if (b_canInteract == true)
-        {
-            if (targetIsInteractive)
-            {
-                if (Input.GetKeyDown(b_boundKey))
-                {
-                    interactiveObject.GetComponent<Interactable>().ButtonPress();
-                }               
-            }
+            buttonObject = null; // Makes it so the player cant press the button while looking away from it
         }
     }
 }
